33d448f8f80dd37ea2bad560dbd3d814a4d6bf38
rafaelcalpena <rafael.calpena@gmail.com>
2020-03-23 02:42:58 -0300

    chore: update wcota gh scraper, use envvars

--- unofficial.js
+++ unofficial.js
@@ -1,5 +1,6 @@
-const repoUrl = 'https://github.com/wcota/covid19br';
-const target = 'unofficial-data';
+const repoUrl = process.env.WCOTA_REPO_URL;
+const target = process.env.WCOTA_REPO_PATH;
+const csvFile = 'cases-brazil-total.csv';
 
 
 const pollUnofficial = () => {
@@ -8,23 +9,33 @@ const pollUnofficial = () => {
     /*  Git clone if repo not present. THen pull */
     require('child_process').execSync(`git -C ${target} pull || git clone ${repoUrl} ${target}`);
 
-    const date = require('child_process').execSync(`cd ${target} && git --no-pager log -1 --pretty="format:%ci" cases-brazil-total.csv`).toString();
+    const date = require('child_process').execSync(`cd ${target} && git --no-pager log -1 --pretty="format:%ci" ${csvFile}`).toString();
     const rDate = new Date(date).toLocaleString("pt-BR")
 
     /* Read latest file */
     const results = []
     let totalUnofficial = null;
+    let deaths = null;
+    let stateInfo = [];
     
     const r = new Promise((resolve, reject) => {
         require('fs').createReadStream(
-            require('path').relative(__dirname, `./${target}/cases-brazil-total.csv`)
+            require('path').relative(__dirname, `./${target}/${csvFile}`)
             ).pipe(
             require('csv-parser')()
         ).on('data', (data) => {
             results.push(data)
         }).on('end', () => {
             totalUnofficial = results[0].totalCases;
-            resolve({newValue: totalUnofficial, date: rDate});
+            deaths = results[0].deaths;
+            stateInfo = results.map(r => {
+                return {
+                    ...r,
+                    totalCases: parseInt(r.totalCases),
+                    deaths: parseInt(r.deaths)
+                }
+            }).sort((a, b) => (a.totalCases > b.totalCases) ? -1 : 1);
+            resolve({newValue: totalUnofficial, deaths, onlyCases: totalUnofficial - deaths, date: rDate, stateInfo});
         })    
     })
 
